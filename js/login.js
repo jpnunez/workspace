03690cc5032e40fefd791a6ea722a832f5e57fa6
jpnunez <68865762+jpnunez@users.noreply.github.com>
2020-08-12 00:15:55 -0300

    javascript informaion de usuario

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var registro = document.getElementById("formulario")
+var email = document.getElementById("email");
+var pass = document.getElementById("password");
 
+registro.addEventListener("submit",function(event){
+    event.preventDefault();
+    let usuarios = Array(
+    {
+        usuario: email.value,
+        contraseña:  pass.value
+    }
+    );
+    localStorage.setItem("usuario",JSON.stringify(usuarios));
+    location.href="index.html";
 });
